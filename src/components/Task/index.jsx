eff353023e817a4030c83d4ff83efbcf5a80e58b
Tanush Kumar <tkumar@zscaler.com>
2023-06-14 15:25:50 +0530

    basic ui

--- src/components/Task/index.jsx
+++ src/components/Task/index.jsx
@@ -1,24 +1,30 @@
-import React from 'react'
-import './styles.scss'
+import React from 'react';
+import './styles.scss';
 
-const Task = ({task, index, deleteTask}) => {
- const {title, description, completed} = task;
-  return (
+const Task = ({ task, index, deleteTask }) => {
+	const { title, description, completed } = task;
+	return (
 		<div className='task'>
-			<div>
-				<div>
+			<div className='task-top'>
+				<div className='task-top-left'>
 					<div>{title}</div>
 					<div>{description}</div>
 				</div>
-				<div>{completed ? 1 : 0}</div>
+				<div className='task-top-right'>{completed ? 1 : 0}</div>
 			</div>
 			<div className='line'></div>
 			<div>
 				<button>Mark as done</button>
-				<button onClick={()=>{deleteTask(index)}}>Del</button>
+				<button
+					onClick={() => {
+						deleteTask(index);
+					}}
+				>
+					Del
+				</button>
 			</div>
 		</div>
-  );
-}
+	);
+};
 
-export default Task
+export default Task;
