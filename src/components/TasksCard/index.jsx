ea0802001943c87ac6fa16e053ab9241b6be2a6f
Tanush Kumar <tkumar@zscaler.com>
2023-06-14 12:03:42 +0530

    TasksList

--- src/components/TasksCard/index.jsx
+++ src/components/TasksCard/index.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Header from '../Header';
+import TasksList from '../TasksList';
 import './styles.scss'
 
 const TasksCard = () => {
+
+    const [tasks, setTasks] = useState([{title: "My first task", description: "This is my info", completed: false}]);
+
   return (
     <div className='tasks-card'>
-
+        <Header/>
+        <TasksList tasks={tasks}/>
     </div>
   )
 }
