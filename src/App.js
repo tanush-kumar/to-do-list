d937309ad2162a785af54e65939482a3ba0f07b0
Tanush Kumar <tkumar@zscaler.com>
2023-06-14 11:53:35 +0530

    Task Card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import './global.scss'
+import './global.scss';
+import TasksCard from './components/TasksCard';
 
 function App() {
-  return (
-    <div className="App">
-      hello
-    </div>
-  );
+	return (
+		<div className='app'>
+			<TasksCard />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/TasksCard/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.scss'
+
+const TasksCard = () => {
+  return (
+    <div className='tasks-card'>
+
+    </div>
+  )
+}
+
+export default TasksCard
